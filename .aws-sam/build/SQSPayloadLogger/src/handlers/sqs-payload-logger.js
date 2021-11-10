10b1c6133a91d8f7052d21f26e3ae967c907ae33
Luis Carlos <luisca_jl@hotmail.com>
2021-11-10 06:24:12 +0000

    Added code

--- .aws-sam/build/SQSPayloadLogger/src/handlers/sqs-payload-logger.js
+++ .aws-sam/build/SQSPayloadLogger/src/handlers/sqs-payload-logger.js
@@ -3,7 +3,9 @@
 var AWS = require("aws-sdk");
 var sns = new AWS.SNS();
 const parseTorrent = require('parse-torrent')
-
+AWS.config.update({ region: 'eu-west-1' });
+var sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+const SQS_QUEUE_URL = process.env.SQS_QUEUE_URL;
 exports.sqsPayloadLoggerHandler = async (event, context) => {
     console.log("Request que ha llegado -> ");
     console.log(event);
@@ -17,20 +19,39 @@ exports.sqsPayloadLoggerHandler = async (event, context) => {
         checkHttp(url);
     } catch (error) {
         console.error(error);
-      }
+    }
     let magnetUri = await convertTorrent(url);
+    let dataToReturn;
     if (magnetUri) {
-        return {
+        console.log("Torrent Validate");
+        dataToReturn = {
             validate: true,
             magnet: magnetUri,
             torrent: url
         }
     } else {
-        return {
+        console.log("Torrent Not Validate");
+        dataToReturn = {
             validate: false,
             torrent: url
         }
     }
+    console.log("Data to return")
+    console.log(JSON.stringify(dataToReturn))
+    var params = {
+        MessageBody: JSON.stringify(dataToReturn),
+        QueueUrl: SQS_QUEUE_URL,
+    };
+    console.log(`Sending notification via SQS: ${SQS_QUEUE_URL}.`);
+    try {
+        await sqs.sendMessage(params).promise(); // since your handler returns a promise, lambda will only resolve after sqs responded with either failure or success
+    } catch (err) {
+        console.log(err)
+    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify(dataToReturn),
+    };
 }
 function convertTorrent(uri) {
     return new Promise(function (resolve, reject) {
