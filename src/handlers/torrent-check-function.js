ce9ae4a5efa5c10d6b772c9b6f9e911f7bb2f789
Luis Carlos <luisca_jl@hotmail.com>
2021-11-16 11:38:39 +0100

    new environments to torrent-check

--- src/handlers/torrent-check-function.js
+++ src/handlers/torrent-check-function.js
@@ -6,7 +6,7 @@ const parseTorrent = require('parse-torrent')
 AWS.config.update({ region: 'eu-west-1' });
 var sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
 const SQS_QUEUE_URL = process.env.SQS_QUEUE_URL;
-exports.sqsPayloadLoggerHandler = async (event, context) => {
+exports.handler = async (event, context) => {
     console.log("Request que ha llegado -> ");
     console.log(event);
     var body = event.Records[0].body;
@@ -37,14 +37,14 @@ exports.sqsPayloadLoggerHandler = async (event, context) => {
         }
     }
     console.log("Data to return")
-    console.log(JSON.stringify(dataToReturn))
+    await console.log(JSON.stringify(dataToReturn))
     var params = {
         MessageBody: JSON.stringify(dataToReturn),
         QueueUrl: SQS_QUEUE_URL,
     };
-    console.log(`Sending notification via SQS: ${SQS_QUEUE_URL}.`);
     try {
-        await sqs.sendMessage(params).promise(); // since your handler returns a promise, lambda will only resolve after sqs responded with either failure or success
+        console.log(`Sending notification via SQS: ${SQS_QUEUE_URL}.`);
+        await sqs.sendMessage(params).promise();
     } catch (err) {
         console.log(err)
     }
